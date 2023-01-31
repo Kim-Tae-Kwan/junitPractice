76a1f794ef3fec0fa92d8aa650327d9ba13e88fc
KTK <vodjaos@gmail.com>
2023-01-31 22:51:55 +0900

    Service 구현 중

--- src/main/java/com/ktk/junitPractice/service/BookService.java
+++ src/main/java/com/ktk/junitPractice/service/BookService.java
@@ -2,7 +2,20 @@ package com.ktk.junitPractice.service;
 
 import org.springframework.stereotype.Service;
 
-@Service
-public class BookService {
+import com.ktk.junitPractice.repository.BookRepository;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+	
+	private final BookRepository bookRepository;
+	
+	
+	// 1. 책 등록
+	// 2. 책 목록보기
+	// 3. 책 한권 보기
+	// 4. 책 삭제
+	// 5. 책 수정
 }
